6b1228f563cccf857f417536f40d4519bed11b1c
Joe Dzado <joe.dzado@gmail.com>
2024-05-04 18:30:00 -0500

    atmoic bool w/tests

--- littlebits.h
+++ littlebits.h
@@ -1,7 +1,30 @@
 #pragma once
+#include <array>
+#include <atomic>
+#include <ranges>
+#include <type_traits>
+
+namespace {
+struct Base {};
+} // namespace
 #define COUNT_ARGS(...) (sizeof((int[]){__VA_ARGS__}) / sizeof(int))
 #define CONFIGS(name, ...)                                                     \
-  struct name {                                                                \
+  struct name final : public Base {                                            \
     enum _configs { __VA_ARGS__ };                                             \
     inline const static size_t size = COUNT_ARGS(__VA_ARGS__);                 \
   };
+
+template <typename T, typename = std::enable_if_t<std::is_base_of_v<Base, T>>>
+class littlebits {
+public:
+  littlebits() : configs{false} {}
+  explicit littlebits(std::array<bool, T::size> bools) {
+    std::ranges::copy(bools, configs.begin());
+  }
+  template <int Config> auto get() { return configs[Config].load(); }
+  template <int Config> auto &atomic() { return configs[Config]; }
+  template <int Config> auto set(bool value) { configs[Config].store(value); }
+
+private:
+  std::array<std::atomic<bool>, T::size> configs;
+};
--- tests.cpp
+++ tests.cpp
@@ -1,12 +1,75 @@
 #include "littlebits.h"
+#include <cassert>
+#include <chrono>
+#include <format>
+#include <future>
 #include <print>
+#include <random>
+
+using namespace std::chrono_literals;
 CONFIGS(MyConfigs, One, Two, Three, Four)
 CONFIGS(MoreConfigs, One, Two, Three, Four, Five, Six, Seven)
 CONFIGS(EvenMoreConfigs, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten)
 
 int main() {
+  assert(MyConfigs::size == 4);
   std::println("Number of configs: {}", MyConfigs::size);
+  assert(MoreConfigs::size == 7);
   std::println("Number of configs: {}", MoreConfigs::size);
+  assert(EvenMoreConfigs::size == 10);
   std::println("Number of configs: {}", EvenMoreConfigs::size);
+
+  littlebits<MyConfigs> configs;
+  assert(!configs.get<MyConfigs::One>());
+  assert(!configs.get<MyConfigs::Two>());
+  assert(!configs.get<MyConfigs::Three>());
+  assert(!configs.get<MyConfigs::Four>());
+
+  littlebits<MoreConfigs> moreConfigs({true, false, true, false, true, false, true});
+  assert(moreConfigs.get<MoreConfigs::One>());
+  assert(!moreConfigs.get<MoreConfigs::Two>());
+  assert(moreConfigs.get<MoreConfigs::Three>());
+  assert(!moreConfigs.get<MoreConfigs::Four>());
+  assert(moreConfigs.get<MoreConfigs::Five>());
+  assert(!moreConfigs.get<MoreConfigs::Six>());
+  assert(moreConfigs.get<MoreConfigs::Seven>());
+
+  auto &atom = moreConfigs.atomic<MoreConfigs::One>();
+  auto time = std::chrono::system_clock::now();
+  auto a2 = std::async(std::launch::async, [&atom, &time]() {
+    std::this_thread::sleep_for(250ms);
+    atom.store(false);
+    time = std::chrono::system_clock::now();
+    atom.notify_all();
+  });
+  std::atomic<bool> expected = true;
+  atom.wait(true);
+
+  auto delta = std::chrono::system_clock::now() - time;
+  std::println("After thread time: {}", delta);
+
+  std::println("Thread complete.");
+
+  // Done waiting, it's false.
+  assert(false == moreConfigs.get<MoreConfigs::One>());
+
+  auto job = [&atom]() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution dis(1, 2);
+    auto duration = std::chrono::milliseconds(dis(gen));
+    std::this_thread::sleep_for(duration);
+    bool expected = false;
+    while (!atom.compare_exchange_strong(expected, true)) {
+      expected = false;
+    }
+    std::println("Thread {} set the bool.", duration);
+    atom.store(false);
+  };
+  atom.store(false);
+  std::thread t1(job);
+  std::thread t2(job);
+  t1.join();
+  t2.join();
   return 0;
 }
